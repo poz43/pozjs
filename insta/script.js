7560d468b1d22f9d6b8fe1f7837d72f722a2baf6
ksenia <arfloger@gmail.com>
2020-04-25 17:26:35 +0300

    Код со 2 практического занятия

--- insta/script.js
+++ insta/script.js
@@ -1,7 +1,7 @@
 let bodyEl = document.querySelector(`body`)
 let cardsEl = document.querySelector('.cards')
 let countCards = 22
-let comments = [
+let deskrs = [
   `Как вам мой лук?`,
   `Красота фотографии не имеет границ`,
   `Настроение: "А я сядууу в кабриолет и уедууу куда-нибудь!.."`,
@@ -13,6 +13,7 @@ let comments = [
   `Что это было?`,
   `Будь крутым или иди домой`
 ]
+let cards = []
 
 function createHeader() {
   return (`<header class="header">
@@ -28,7 +29,7 @@ function createCard(index, randomLikes, randomComments) {
     `<div class="card">
             <img src="photos/${index}.jpg" alt="">
             <div class="hover">
-              <div class="hover__wrap">
+              <div class="hover__wrap" id="${index - 1}">
                 <div class="likes">${randomLikes}</div>
                 <div class="comments">${randomComments}</div>
               </div>
@@ -41,38 +42,53 @@ function getRandomNumber(min, max) {
   return Math.floor(min - 0.5 + Math.random() * (max - min + 1))
 }
 
-function generateCards() {
+function createCards() {
   for (let i = 0; i < countCards; i++) {
-    let countLikes = getRandomNumber(20, 150)
-    let countComments = getRandomNumber(3, 15)
-    cardsEl.insertAdjacentHTML(`beforeEnd`, createCard(i + 1, countLikes, countComments))
+    let card = {}
+    card.likes = getRandomNumber(20, 150)
+    card.comments = getRandomNumber(3, 15)
+    card.deskr = deskrs[getRandomNumber(1, 9)]
+    card.id = i
+    cards.push(card)
   }
 }
 
+function insertCards() {
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.insertAdjacentHTML(`beforeEnd`, createCard(i + 1, cards[i].likes, cards[i].comments))
+  }
+}
 
+let modal = document.querySelector('.modal-wrap')
+let closeBtn = document.querySelector('.close')
+
+document.addEventListener('click', function (evt) {
+  if (evt.target.classList.contains('hover__wrap')) {
+    let currentId = evt.target.getAttribute('id')
+
+    let likes = modal.querySelector('.likes')
+    let comments = modal.querySelector('.comments')
+    let image = modal.querySelector('img')
+    let deskr = modal.querySelector('p')
+
+    image.src = `photos/${+currentId + 1}.jpg`
+    likes.textContent = cards[currentId].likes
+    comments.textContent = cards[currentId].comments
+    deskr.textContent = cards[currentId].deskr
+
+    modal.classList.add('open')
+
+  }
+});
+
+closeBtn.addEventListener('click', function () {
+  modal.classList.remove('open')
+})
 
 function init() {
   bodyEl.insertAdjacentHTML(`afterBegin`, createHeader())
-  generateCards()
+  createCards()
+  insertCards()
 }
 
 init()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
